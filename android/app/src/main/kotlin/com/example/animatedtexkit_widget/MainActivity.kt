4bba35faae113660f5aa7487f592eb9b8bccbce6
MRMostakim1406204 <mdmrmostakim1406204@gmail.com>
2023-10-23 10:50:27 +0600

    AnimatedTextKit-Widget

---
+++ android/app/src/main/kotlin/com/example/animatedtexkit_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animatedtexkit_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,33 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animated Text Kit'),
+        centerTitle: true,
+      ),
+      body: SafeArea(child: Center(
+        child: AnimatedTextKit(
+          animatedTexts: [
+            WavyAnimatedText("MR Mostakim",textStyle: TextStyle(
+              fontSize: 20,color: Colors.pink
+            ),speed: Duration(milliseconds: 500),
+            //textAlign: TextAlign.end
+            )
+            
+          ]),
+      )),
+    );
+  }
+}
